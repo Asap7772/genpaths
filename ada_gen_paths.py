4c70073b9fc5efae01489a82907a58b278c3c816
Anikait Singh <singh.anikait@gmail.com>
2022-09-12 18:14:19 -0700

    Update ada_gen_paths.py

--- ada_gen_paths.py
+++ ada_gen_paths.py
@@ -4,87 +4,89 @@ import json
 from tabnanny import check
 
 main_folder = '/home/aviralkumar/hdd/'
-which_day = 'microwave_bc'
+which_days = ('dueling_tk6_maxadv', 'dueling_tk6_pretrain', 'dueling_pretrain_awbcsep', 'dueling_tk6_cqlonly_weightedtanh', 'dueling_tk6_joint')
 cmd_file = 'scp_cmd.sh'
 output_dir = '/raid/asap7772/microwave_bc/'
+ 
+output_file = open(f'{main_folder}/{cmd_file}', 'w+')
 
-# which_checkpoints=[80000, 100000, 120000, 140000, 160000, 180000, 240000]
-which_checkpoints=[120000, 180000, 220000, 240000, 260000, 320000, 360000, 440000, 480000]
+for which_day in which_days:
+    print(f'Path = {which_day}')
+    # which_checkpoints=[80000, 100000, 120000, 140000, 160000, 180000, 240000]
+    which_checkpoints=[120000, 180000, 220000, 240000, 260000, 320000, 360000, 440000, 480000]
 
-check_json=dict(
-    #  target_dataset={'toykitchen6_knife_in_pot'},
-)
-check_train_json=dict(
-   
-)
+    check_json=dict(
+        #  target_dataset={'toykitchen6_knife_in_pot'},
+    )
+    check_train_json=dict(
 
-sub_path=os.path.join(main_folder, which_day)
+    )
 
-all_dirs = set()
-for root, dirs, files in os.walk(sub_path, topdown=False):
-   for name in files:
-      f_name = os.path.join(root, name)
-      if 'checkpoint' in f_name:
-        all_dirs.add(f_name.split('checkpoint')[0])
+    sub_path=os.path.join(main_folder, which_day)
 
-output_file = open(f'{main_folder}/{cmd_file}', 'w')
+    all_dirs = set()
+    for root, dirs, files in os.walk(sub_path, topdown=False):
+       for name in files:
+          f_name = os.path.join(root, name)
+          if 'checkpoint' in f_name:
+            all_dirs.add(f_name.split('checkpoint')[0])
 
-i=0
-for dir in all_dirs:
-    conf = f'{dir}config.json'
-    try:
-        with open(conf) as f:
-            data = json.load(f)
-    except:
-        continue
-    
-    things_to_print=dict()
+    i=0
+    for dir in all_dirs:
+        conf = f'{dir}config.json'
+        try:
+            with open(conf) as f:
+                data = json.load(f)
+        except:
+            continue
 
-    filter=False
-    for x in check_json:
-        if x in data and data[x] not in check_json[x]:
-            filter=True
-    for x in check_train_json:
-        things_to_print[x] = data['train_kwargs'][x]
-        if x in data['train_kwargs'] and data['train_kwargs'][x] not in check_train_json[x]:
-            filter=True
+        things_to_print=dict()
 
-    if not filter:
-        print(i, dir)
-        i += 1
+        filter=False
+        for x in check_json:
+            if x in data and data[x] not in check_json[x]:
+                filter=True
+        for x in check_train_json:
+            things_to_print[x] = data['train_kwargs'][x]
+            if x in data['train_kwargs'] and data['train_kwargs'][x] not in check_train_json[x]:
+                filter=True
 
-        for x in things_to_print:
-            print(x, things_to_print[x]) 
+        if not filter:
+            print(i, dir)
+            i += 1
 
-        print()
-        
-        output_file.write(f"echo 'Copying {dir}' \n")
-        folder_to_make = f'{output_dir}/{dir.split(sub_path)[-1]}'
-        mkdir_command = f"ssh ada 'mkdir -p {folder_to_make}' \n"
-        output_file.write(mkdir_command)
+            for x in things_to_print:
+                print(x, things_to_print[x]) 
 
-        fi='config.json'
-        scp_command = f'rsync -vae ssh {dir}/{fi} ada:{folder_to_make} \n'
-        output_file.write(scp_command)
+            print()
 
-        for fi in os.listdir(dir):
-            if 'checkpoint' in fi and fi != 'checkpointtmp':
-                which = int(fi.split('checkpoint')[-1])
-                if which in which_checkpoints or which % 100000 == 0:
-                    print(f'added scp command for {which}')
+            output_file.write(f"echo 'Copying {dir}' \n")
+            folder_to_make = f'{output_dir}/{dir.split(sub_path)[-1]}'
+            mkdir_command = f"ssh ada 'mkdir -p {folder_to_make}' \n"
+            output_file.write(mkdir_command)
 
-                    scp_command = f'rsync -vae ssh {dir}/{fi} ada:{folder_to_make} \n'
-                    output_file.write(scp_command)
-                    # print(scp_command)      
-        
-        output_file.write('\n')
-        
-        print()
-        print(data['train_kwargs'])
-        print()
-    # print(x.split('/')[-2])
-    # print()
+            fi='config.json'
+            scp_command = f'rsync -vae ssh {dir}/{fi} ada:{folder_to_make} \n'
+            output_file.write(scp_command)
 
-print(f'total={i}')
-print(output_file)
-output_file.close()
+            for fi in os.listdir(dir):
+                if 'checkpoint' in fi and fi != 'checkpointtmp':
+                    which = int(fi.split('checkpoint')[-1])
+                    if which in which_checkpoints or which % 100000 == 0:
+                        print(f'added scp command for {which}')
+
+                        scp_command = f'rsync -vae ssh {dir}/{fi} ada:{folder_to_make} \n'
+                        output_file.write(scp_command)
+                        # print(scp_command)      
+
+            output_file.write('\n')
+
+            print()
+            print(data['train_kwargs'])
+            print()
+        # print(x.split('/')[-2])
+        # print()
+
+    print(f'total={i}')
+    print(output_file)
+    output_file.close()
