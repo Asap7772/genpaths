5cd57eb489d0ae7db96aa5ad37478e24996a3583
Anikait Singh <singh.anikait@gmail.com>
2022-09-20 22:30:57 -0700

    Update ada_gen_paths.py

--- ada_gen_paths.py
+++ ada_gen_paths.py
@@ -8,7 +8,7 @@ home = expanduser("~")
 main_folder = f'{home}/hdd/'
 # which_days = ('sept15_no_all_sarsa', 'sept15_sarsa_pretrain_no_detach', 'sept14_sarsa_pretrain', 'sept14_sarsa_pretrain_all_cql_finetune')
 # main_folder = '/home/anikaitsingh/hdd_quan/'
-which_days = ('mae_baseline',)
+which_days = ('09_20_cvae_actionprediction_fixupdate',)
 
 cmd_file = 'scp_cmd.sh'
 output_dir = '/raid/asap7772/exps_sept20/'
